30f676a82ec10c1e1e0f1838a548eebc13fbe31d
Anonymous-Chris <sk.niyara@gmail.com>
2020-12-12 19:18:41 -0600

    increment method

--- src/components/Componenttwo.vue
+++ src/components/Componenttwo.vue
@@ -1,26 +1,8 @@
 <template>
 <div class="menu">
-   <div v-bind:class="{active:isActive,'text-danger': hasError }">
-       <!--Mustaches cannot be used inside HTML attributes. Instead, use a v-bind directive:
-        example:  <div v-bind:id="dynamicId"></div>
-        -->
-       <h1>{{name}}</h1>
 
-        <h2 @click="returnReverse(name)">{{returnReverse(name)}} yo</h2>
-       <p v-if="seen">yes you can see me</p>
-   </div>
-<!-- v-model is used for two way binding. It means if one of the value changes ,
-both the values are changed. -->
-   <input v-model="counter">
-    <h1>{{counter}}</h1>
-
-    <div>
-        <ol>
-            <li v-for="todo in todos" v-bind:key="todo.text">
-                {{todo.text}}
-            </li>
-        </ol>
-    </div>
+<button v-on:click="increment">Click to increment</button>
+<p>{{counter}}</p>
 </div>
 </template>
 
@@ -34,34 +16,16 @@ export default {
     },
     data:()=>{
         return({
-
-            name:"hero panti",
-            seen:false,
-            reversename:"name",
-            isActive:false,
-             hasError: true,
              counter:0,
-                   todos: [
-        { text: 'Learn JavaScript' },
-        { text: 'Learn Vue' },
-        { text: 'Build something awesome' }
-      ]
             })
     },methods:{
-        returnReverse(a){
-         //   console.log(a.split('').reverse().join(''))
-           // console.log(this.seen)
-            return (a.split('').reverse().join(''))
-    
+        increment(){
+            return this.counter+=1;
         }
     },
-    mounted(){
-        setInterval(()=>{
-            this.counter++
-            //console.log(this.name)
-        },1000)
+
     }
-}
+
 </script>
 
 
