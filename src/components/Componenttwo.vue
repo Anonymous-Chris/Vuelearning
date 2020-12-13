21969223d11a5e8765a316101aabb0dffd773858
Anonymous-Chris <sk.niyara@gmail.com>
2020-12-12 23:49:55 -0600

    computed properties in action

--- src/components/Componenttwo.vue
+++ src/components/Componenttwo.vue
@@ -3,6 +3,9 @@
 
 <button v-on:click="increment">Click to increment</button>
 <p>{{counter}}</p>
+<p>{{author.books[0]}}</p>
+<p>No of books written by {{author.name}} is: {{noofBooks}}</p>
+<!--the data can be accessed in the form of objects as well in the template -->
 </div>
 </template>
 
@@ -17,12 +20,25 @@ export default {
     data:()=>{
         return({
              counter:0,
+             author:{
+                 name:'kshitij',
+                 books:[
+                     'hero panti',
+                     'maya nai maya',
+                     'phul maya',
+                     'christina'
+                 ]
+             }
             })
     },methods:{
         increment(){
             return this.counter+=1;
         }
-    },
+    },computed:{
+        noofBooks(){
+            return this.author.books.length
+        }
+    }
 
     }
 
