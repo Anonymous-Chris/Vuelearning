dc07858d2b6392bdecd7cc14eca4689d272c679a
Anonymous-Chris <sk.niyara@gmail.com>
2020-12-15 20:08:51 -0600

    two way binding using multiple v-models

---
+++ src/components/ComponentsAd.vue
@@ -0,0 +1,34 @@
+<template>
+    <div >
+        <h1>Components In Depth </h1>
+       <li v-for="p in propVal" v-bind:key="p.id">
+           {{p.id}} {{p}}
+       </li>
+
+       <li>
+           <p>object</p>   
+                   {{propVal2.name}}
+       </li>
+    </div>
+</template>
+
+<script>
+export default {
+    data:()=>{
+        return ({
+
+        })
+    },
+    props:{
+        propVal:[],
+        propVal2:{}
+    },
+    methods:{
+     
+    }
+}
+</script>
+
+<style>
+
+</style>
